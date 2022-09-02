deaee8a50190b58d5bd17068602ceed9a7d392d7
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-09-02 14:34:33 -0700

    fixing betty

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,19 +8,19 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if(!tree)
-		return(0);
+	if (!tree)
+		return (0);
 	
-	if(!tree->left && !tree->right)
-		return(0);
+	if (!tree->left && !tree->right)
+		return (0);
 
-	if(!tree->left)
-		return(-1 - binary_tree_height(tree->right));
+	if (!tree->left)
+		return (-1 - binary_tree_height(tree->right));
 
-	if(!tree->right)
-		return(binary_tree_height(tree->left) - (-1));
+	if (!tree->right)
+		return (binary_tree_height(tree->left) - (-1));
 
-	return(binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
 
 /**
@@ -31,17 +31,17 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t count_left = 0, count_right = 0;
+	size_t count_left = 0, count_right = 0;
+	
+	if (!tree)
+		return (0);
 
-        if (!tree)
-                return (0);
+	if (tree->left)
+		count_left = 1 + binary_tree_height(tree->left);
 
-        if (tree->left)
-                count_left = 1 + binary_tree_height(tree->left);
-
-        if (tree->right)
-                count_right = 1 + binary_tree_height(tree->right);
-
-        return (count_left > count_right ? count_left : count_right);
+	if (tree->right)
+		count_right = 1 + binary_tree_height(tree->right);
+	
+	return (count_left > count_right ? count_left : count_right);
 }
 
