a210fa9515c79f839a5219d8f202781eff8656c5
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-09-02 14:39:07 -0700

    fixing betty 2

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -32,7 +32,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t count_left = 0, count_right = 0;
-	
+
 	if (!tree)
 		return (0);
 
